3e7cabcc16b98531a6943980effe8069aac16ca7
semenov evgeny <emsemenov@vtb.ru>
2021-11-09 18:32:43 +0400

    adds object config

---
+++ config.go
@@ -0,0 +1,49 @@
+package mqpro
+
+import (
+  "github.com/pkg/errors"
+  "gopkg.in/yaml.v3"
+  "io/ioutil"
+)
+
+type TransportCfg struct {
+  Alias  string `yaml:"alias"`
+  Browse string `yaml:"browse"`
+  Put    string `yaml:"put"`
+  Get    string `yaml:"get"`
+}
+
+type Config struct {
+  LogMode   string         `yaml:"logMode"`
+  LogCli    bool           `yaml:"logCli"`
+  LogLevel  string         `yaml:"logLevel"`
+  DevMode   bool           `yaml:"devMode"`
+  Host      string         `yaml:"host"`
+  Port      int            `yaml:"port"`
+  Manager   string         `yaml:"manager"`
+  Channel   string         `yaml:"channel"`
+  App       string         `yaml:"app"`
+  User      string         `yaml:"user"`
+  Pass      int            `yaml:"pass"`
+  Header    string         `yaml:"header"`
+  RootTag   interface{}    `yaml:"rootTag"`
+  Transport []TransportCfg `yaml:"transport"`
+}
+
+func (p *Mqpro) ParseConfig(f string) error {
+  if f == "" {
+    return ErrConfigPathEmpty
+  }
+  byt, err := ioutil.ReadFile(f)
+  if err != nil {
+    return errors.Wrap(err, "Error reading configuration file")
+  }
+
+  var c Config
+  err = yaml.Unmarshal(byt, c)
+  if err != nil {
+    return errors.Wrapf(err, "Configuration file parsing error '%s'", f)
+  }
+
+  return nil
+}
