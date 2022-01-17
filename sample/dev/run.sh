1957b6cf72fd17f6fc2208aa16221c5f82978e5b
semenov evgeny <emsemenov@vtb.ru>
2022-01-17 13:37:55 +0300

    sample: setting up ibm mq managers

--- sample/dev/run.sh
+++ sample/dev/run.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
 BIN=$(dirname "$([[ $0 == /* ]] && echo "$0" || echo "$PWD/${0#./}")")
+TMP=$(mktemp)
+
+sed -n 19,100p "$0" > "$TMP"
 
 go get
 
 while true; do
-  bash "${BIN}/run2.sh" "$1"
+  bash "$TMP" "${BIN}/$1"
+  sleep 2
 done
 
+exit 0
+
+# Код временного файла для запуска приложения
+# ------------------------------------
+
+#!/bin/bash
+
+BIN=$(dirname "$([[ $0 == /* ]] && echo "$0" || echo "$PWD/${0#./}")")
+
+CFG=$1
+
+echo
+echo "###########################################################"
+echo "# Старт в DEV MODE                                        #"
+echo "# [ctrl + c] - два раза подряд для выхода                 #"
+echo "# файл конфигурации = $CFG"
+echo "###########################################################"
+
+[ ! -f "$CFG" ] && echo "нет файла конфигурации: '$CFG'" && exit 1
+
+set -o allexport
+source "$CFG"
+set +o allexport
+
+/usr/local/go-1.16.4/bin/go run *.go
